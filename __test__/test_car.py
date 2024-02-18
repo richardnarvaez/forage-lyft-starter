952962a44fc9e9e61ec6f43a464212946239d341
Richard V. Narvaez <richard.bvn997@gmail.com>
2024-02-18 16:07:23 -0500

    refactor: car tests and use CarFactory

--- __test__/test_car.py
+++ __test__/test_car.py
@@ -1,44 +1,42 @@
 import unittest
 from datetime import datetime
 
+from car.car_factory import CarFactory
+
 class TestCalliope(unittest.TestCase):
     def test_battery_should_be_serviced(self):
-        # today = datetime.today().date()
-        # last_service_date = today.replace(year=today.year - 3)
-        # current_mileage = 0
-        # last_service_mileage = 0
+        today = datetime.today().date()
+        last_service_date = today.replace(year=today.year - 3)
+        current_mileage = 0
+        last_service_mileage = 0
 
-        # car = Calliope(last_service_date, current_mileage, last_service_mileage)
-        # self.assertTrue(car.needs_service())
-        self.assertTrue(False)
+        calliope_car = CarFactory.create_calliope(today, last_service_date, current_mileage, last_service_mileage)
+        self.assertTrue(calliope_car.needs_service())
 
     def test_battery_should_not_be_serviced(self):
-        # today = datetime.today().date()
-        # last_service_date = today.replace(year=today.year - 1)
-        # current_mileage = 0
-        # last_service_mileage = 0
+        today = datetime.today().date()
+        last_service_date = today.replace(year=today.year - 1)
+        current_mileage = 0
+        last_service_mileage = 0
 
-        # car = Calliope(last_service_date, current_mileage, last_service_mileage)
-        # self.assertFalse(car.needs_service())
-        self.assertTrue(False)
+        calliope_car = CarFactory.create_calliope(today, last_service_date, current_mileage, last_service_mileage)
+        self.assertFalse(calliope_car.needs_service())
 
     def test_engine_should_be_serviced(self):
-        # last_service_date = datetime.today().date()
-        # current_mileage = 30001
-        # last_service_mileage = 0
+        last_service_date = datetime.today().date()
+        current_mileage = 30001
+        last_service_mileage = 0
 
-        # car = Calliope(last_service_date, current_mileage, last_service_mileage)
-        # self.assertTrue(car.needs_service())
-        self.assertTrue(False)
+        calliope_car = CarFactory.create_calliope(datetime.today().date(), last_service_date, current_mileage, last_service_mileage)
+        self.assertTrue(calliope_car.needs_service())
 
     def test_engine_should_not_be_serviced(self):
-        # last_service_date = datetime.today().date()
-        # current_mileage = 30000
-        # last_service_mileage = 0
+        last_service_date = datetime.today().date()
+        current_mileage = 30000
+        last_service_mileage = 0
 
-        # car = Calliope(last_service_date, current_mileage, last_service_mileage)
-        # self.assertFalse(car.needs_service())
-        self.assertTrue(False)
+        calliope_car = CarFactory.create_calliope(datetime.today().date(), last_service_date, current_mileage, last_service_mileage)
+        self.assertFalse(calliope_car.needs_service())
 
 if __name__ == '__main__':
     unittest.main()
